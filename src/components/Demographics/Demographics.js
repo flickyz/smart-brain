c72c39e3de91480bc04ff5c769b68b07176fa229
flicky <flicky@flicky-MacBook-Pro.local>
2018-05-26 18:39:49 +1200

    face recognition function added

--- src/components/Demographics/Demographics.js
+++ src/components/Demographics/Demographics.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Demographics.css';
 
-const Demographics = ({ imageUrl }) => {
+const Demographics = ({ imageUrl, box }) => {
 	return (
 		<div className = 'center ma'>
-			<div className = 'mt2'>
-				<img alt = 'random' src = {imageUrl} width = '700px' height = 'auto' />
+			<div className = 'relative mt2'>
+				<img 
+					id = 'inputimage' 
+					alt = '' 
+					src = {imageUrl} 
+					width = '700px' 
+					height = 'auto'
+				/>
+				<div
+					className = 'facebox ba bw1 blue' 
+					style = {{
+							left: box.left,
+						 	top: box.top,
+						 	right: box.right,
+						 	bottom: box.bottom
+					}}>
+				</div>
+				
 			</div>
 		</div>
 		);
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,15 +7,17 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 			<p className = 'f3'>
                 {'This Magic Brain will detect faces in your pictures. Give it a try.'}
             </p>
-			<div className = 'center form pa3 br3'>
-				<input 
-					className = 'w-80 f4 pa2' 
-					type = 'text'
-					onChange = {onInputChange} />
-				<button 
-					className = 'w-20 grow f4 br3 link ph3 pv2 dib white'
-					onClick = { onButtonSubmit }
-					>Detect</button>
+			<div className = 'center'>
+				<div className = 'form pa3 br3'>
+					<input 
+						className = 'w-80 f4 pa2' 
+						type = 'text'
+						onChange = {onInputChange} />
+					<button 
+						className = 'w-20 grow f4 br3 link ph3 pv2 dib white'
+						onClick = { onButtonSubmit }
+						>Detect</button>
+				</div>
 			</div>
 		</div>
 	);
